5b1eedd95def55d12eceecb467577022f4bbc9c2
Chris Ruff <Chrisruff@live.ca>
2020-04-01 22:45:25 -0300

    Began work on testing framework

---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "../src/Search.h"
+
+using namespace MLibs::Tester;
+
+TEST_CASE("Testing AStar Search")
+{
+	CHECK(findEasyPath);
+}
