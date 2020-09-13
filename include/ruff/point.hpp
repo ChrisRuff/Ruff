8d46c8625badb9834d36f3cc78c8b62148fad969
Chris Ruff <Chrisruff@live.ca>
2020-09-12 23:54:44 -0300

    Added C++20 stuff to point and fixed the test target

--- include/ruff/point.hpp
+++ include/ruff/point.hpp
@@ -1,13 +1,14 @@
 #pragma once
 #include <ostream>
-#include <type_traits>
+#include <cmath> // pow
 
-//TODO c++2a:
-//#include <compare>
+#include <compare> // partial_ordering
+#include <concepts> // requires
+#include <type_traits> // is_arithmetic_v
+
 namespace ruff
 {
-//TODO c++2a:
-template<typename T>//requires std::is_arithmetic<T>
+template<typename T> requires std::is_arithmetic_v<T>
 class Point2D
 {
  public:
@@ -16,20 +17,19 @@ class Point2D
 
 
  public:
-	Point2D(const T x, const T y) noexcept : x(x), y(y)
+	[[nodiscard]] Point2D(const T x, const T y) noexcept : x(x), y(y)
 	{
 	}
 	Point2D() = default;
 
-	//TODO c++2a:
-	/*
-		inline constexpr std::partial_ordering operator<=>(const Point2D<T>& other) const
-		{
-			double length1 = std::pow(x,2) + std::pow(y,2);
-			double length2 = std::pow(other.x,2) + std::pow(other.y,2);
-			return (length1 <=> length2);
-		}
-		*/
+	inline constexpr auto operator<=>(const Point2D<T>& other) const noexcept
+	{
+		// Compare based on distance from origin
+		double length1 = std::pow(x,2) + std::pow(y,2);
+		double length2 = std::pow(other.x,2) + std::pow(other.y,2);
+		return (length1 <=> length2);
+	}
+
 	inline friend std::ostream& operator<<(std::ostream& os, const Point2D<T>& point)
 	{
 		return (os << "[" << point.x << ", " << point.y << "]" << std::endl);
